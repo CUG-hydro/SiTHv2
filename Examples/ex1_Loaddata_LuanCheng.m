3dced13cdd4500ace89cd17de8185b46c94b0d83
Dongdong Kong <kongdd@users.noreply.github.com>
2024-10-19 15:21:31 +0800

    change file dir

--- Examples/ex1_Loaddata_LuanCheng.m
+++ Examples/ex1_Loaddata_LuanCheng.m
@@ -5,23 +5,23 @@ j = 2947;
 % 0.1 deg
 Soilraster = load('inpara\Soilraster.mat').Soilraster;
 soil_type = Soilraster(i, j);
+soilpar = get_soilpar(soil_type);
 
-maskland = load('inpara\landmask01.mat').mask2;
-maskland = maskland(i, j);
+% maskland = load('inpara\landmask01.mat').mask2;
+% maskland = maskland(i, j);
 
 % Load the optimal temperature for plant growth
 Topt = load('inpara\Topt.mat');
 Topt = single(Topt.Topt_new);
 Topt = Topt(i, j);
+Top = Topt;
 
 df = readtable("data/dat_栾城_ERA5L_1982-2019.csv");
 nyear = 2016 - 1988 + 1;
 
 %% model parameters 
-Top = Topt;
 PFTi = 22; % Land cover type
 pftpar = get_pftpar(PFTi);
-soilpar = get_soilpar(soil_type);
 
 % retrieve data
 dates = df.date;
---
+++ Examples/ex2_Loaddata.m
@@ -0,0 +1,41 @@
+% 针对CRO进行参数优化
+Soilraster = load('inpara\Soilraster.mat').Soilraster;
+
+cellsize=0.1;
+LON = -180+cellsize/2:cellsize:180;
+LAT = flip(-90+cellsize/2:cellsize:90);
+
+% three global variables
+st = readtable("data/siteInfo_CRO_5sp.csv");
+ra_soil = load('inpara\Soilraster.mat').Soilraster; % LAT, LON
+ra_Topt = single(load('inpara\Topt.mat').Topt_new);
+
+k = 1;
+[soilpar, Topt, pftpar, state] = init_param(k);
+
+% 根据土壤属性，对参数进行初始化。
+% 一开始对田间进行漫灌，各层从fc开始。
+function [soilpar, Topt, pftpar, state] = init_param(k)
+    x = st.lon(k);
+    y = st.lat(k);
+    [i, j] = findnear(LON, LAT, x, y);
+
+    soil_type = ra_soil(j, i);
+    soilpar = get_soilpar(soil_type);
+
+    Topt = ra_Topt(j, i);
+
+    PFTi = 22; % Land cover type
+    pftpar = get_pftpar(PFTi);
+
+    theta_sat = soilpar(3); % saturated wa
+    sm = ones(3, 1) * theta_sat;
+    zg = 0;
+    snowpack = 0;
+    state = mSiTH.update_state(struct(), sm, zg, snowpack);
+end
+
+function [i, j] = findnear(LON, LAT, x, y)
+    [~, i] = min(abs(LON - x));
+    [~, j] = min(abs(LAT - y));
+end
